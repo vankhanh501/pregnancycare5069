04f081cf9e63f3fbb9a43cad83858b2dff0f7238
vankhanh501 <vankhanhdn2021@gmail.com>
2026-06-27 14:47:58 +0700

    Update approve.js

--- api/approve.js
+++ api/approve.js
@@ -1,18 +1,15 @@
-javascript
 export default async function handler(req, res) {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-
     const { paymentId } = req.body;
     const auth = req.headers.authorization;
-
-    const r = await fetch('https://api.minepi.com/v2/payments/' + paymentId + '/approve', {
+    
+    const response = await fetch('https://api.minepi.com/v2/payments/' + paymentId + '/approve', {
         method: 'POST',
         headers: {
             'Authorization': auth,
             'Content-Type': 'application/json'
         }
     });
-
-    const d = await r.json();
-    res.status(r.status).json(d);
+    
+    const data = await response.json();
+    res.status(response.status).json(data);
 }
