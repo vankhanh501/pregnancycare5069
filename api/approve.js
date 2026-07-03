aaacab8a142f5fae3b798afd22cadb461fd3047e
vankhanh501 <vankhanhdn2021@gmail.com>
2026-07-03 10:21:56 +0700

    Add payment approval handler in approve.js

---
+++ api/approve.js
@@ -0,0 +1,10 @@
+export default async function handler(req, res) {
+    const r = await fetch('https://api.minepi.com/v2/payments/' + req.body.paymentId + '/approve', {
+        method: 'POST',
+        headers: {
+            'Authorization': req.headers.authorization,
+            'Content-Type': 'application/json'
+        }
+    });
+    res.status(r.status).json(await r.json());
+}
