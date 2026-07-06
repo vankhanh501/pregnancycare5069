beb38629b8ebaf3e4bd918e446eafb598615f7c9
vankhanh501 <vankhanhdn2021@gmail.com>
2026-07-06 17:14:08 +0700

    Simplify error response in approve.js
    Remove stack trace from error response

--- api/approve.js
+++ api/approve.js
@@ -24,6 +24,6 @@ export default async function handler(req, res) {
         return res.status(response.status).json(data);
 
     } catch (error) {
-        return res.status(500).json({ error: error.message, stack: error.stack });
+        return res.status(500).json({ error: error.message });
     }
 }
