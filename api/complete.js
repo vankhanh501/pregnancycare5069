84ebe325d7d6a169096043b7b3eb7c17ab56b8db
vankhanh501 <vankhanhdn2021@gmail.com>
2026-07-06 16:53:45 +0700

    Refactor complete.js to use async/await

--- api/complete.js
+++ api/complete.js
@@ -1,53 +1,29 @@
 javascript
-const https = require('https');
-
-module.exports = (req, res) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
-    res.setHeader('Access-Control-Allow-Headers', 'Authorization, Content-Type');
-
-    if (req.method === 'OPTIONS') {
-        return res.status(200).end();
-    }
-
-    if (req.method !== 'POST') {
-        return res.status(405).json({ error: 'Method not allowed' });
-    }
-
-    const paymentId = req.body.paymentId;
-    const txid = req.body.txid;
-    const auth = req.headers.authorization;
-
-    const postData = JSON.stringify({});
-
-    const options = {
-        hostname: 'api.minepi.com',
-        path: '/v2/payments/' + paymentId + '/complete',
-        method: 'POST',
-        headers: {
-            'Authorization': auth,
-            'Content-Type': 'application/json',
-            'Content-Length': Buffer.byteLength(postData)
+export default async function handler(req, res) {
+    try {
+        if (req.method !== 'POST') {
+            return res.status(405).json({ error: 'Method not allowed' });
         }
-    };
 
-    const request = https.request(options, (response) => {
-        let data = '';
-        response.on('data', (chunk) => { data += chunk; });
-        response.on('end', () => {
-            try {
-                const json = JSON.parse(data);
-                return res.status(response.statusCode).json(json);
-            } catch (e) {
-                return res.status(500).json({ error: 'Parse error' });
-            }
+        const { paymentId, txid } = req.body;
+
+        if (!paymentId) {
+            return res.status(400).json({ error: 'Missing paymentId' });
+        }
+
+        const response = await fetch('https://api.minepi.com/v2/payments/' + paymentId + '/complete', {
+            method: 'POST',
+            headers: {
+                'Authorization': req.headers.authorization,
+                'Content-Type': 'application/json'
+            },
+            body: '{}'
         });
-    });
 
-    request.on('error', (error) => {
-        return res.status(500).json({ error: error.message });
-    });
+        const data = await response.json();
+        return res.status(response.status).json(data);
 
-    request.write(postData);
-    request.end();
-};
+    } catch (error) {
+        return res.status(500).json({ error: error.message, stack: error.stack });
+    }
+}
